d39f45aa32933bd78469662e414eeacd4f6406fe
Bruno Corrêa <brunosantos6ft@gmail.com>
2024-04-07 12:36:53 -0300

    chore: New assets

--- app/utils/posts.ts
+++ app/utils/posts.ts
@@ -1,7 +1,36 @@
-import Picture1 from './../../public/sample-1.jpg'
-import Picture2 from './../../public/sample-2.jpg'
-import Picture3 from './../../public/sample-3.jpg'
-import Picture4 from './../../public/sample-4.jpg'
+import Picture1 from './../../public/bake-1.jpg'
+import Picture2 from './../../public/bake-2.jpg'
+import Picture3 from './../../public/bake-3.jpg'
+import Picture4 from './../../public/bake-4.jpg'
+import Picture5 from './../../public/brigs-1.jpg'
+import Picture6 from './../../public/brigs-2.jpg'
+import Picture7 from './../../public/brigs-3.jpg'
+import Picture8 from './../../public/brigs-4.jpg'
+import Picture9 from './../../public/brigs-5.jpg'
+import Picture10 from './../../public/brigs-6.jpg'
+import Picture11 from './../../public/brigs-7.jpg'
+import Picture12 from './../../public/brigs-8.jpg'
+import Picture13 from './../../public/brigs-9.jpg'
+import Picture14 from './../../public/cake-1.jpg'
+import Picture15 from './../../public/easter-1.jpg'
+import Picture16 from './../../public/easter-2.jpg'
+import Picture17 from './../../public/easter-3.jpg'
+import Picture18 from './../../public/easter-4.jpg'
+import Picture19 from './../../public/easter-5.jpg'
+import Picture20 from './../../public/gift-1.jpg'
+import Picture21 from './../../public/gift-2.jpg'
+import Picture22 from './../../public/gift-3.jpg'
+import Picture23 from './../../public/gift-4.jpg'
+import Picture24 from './../../public/gift-5.jpg'
+import Picture25 from './../../public/gift-6.jpg'
+import Picture26 from './../../public/gift-7.jpg'
+import Picture27 from './../../public/gift-8.jpg'
+import Picture28 from './../../public/gift-9.jpg'
+import Picture29 from './../../public/pie-1.jpg'
+import Picture30 from './../../public/pie-2.jpg'
+import Picture31 from './../../public/pie-3.jpg'
+import Picture32 from './../../public/pie-4.jpg'
+import Picture33 from './../../public/pie-5.jpg'
 
 export const POSTS = [
   {
@@ -14,7 +43,7 @@ export const POSTS = [
     id: '2',
     title: 'Beautiful cake 2',
     description: 'A beautiful description',
-    image: Picture4,
+    image: Picture2,
   },
   {
     id: '3',
@@ -32,36 +61,174 @@ export const POSTS = [
     id: '5',
     title: 'Beautiful cake 2',
     description: 'A beautiful description',
-    image: Picture4,
+    image: Picture5,
   },
   {
     id: '6',
     title: 'Beautiful cake 4',
     description: 'A beautiful description',
-    image: Picture2,
+    image: Picture6,
   },
   {
     id: '7',
     title: 'Beautiful cake 3',
     description: 'A beautiful description',
-    image: Picture3,
+    image: Picture7,
   },
   {
     id: '8',
     title: 'Beautiful cake 2',
     description: 'A beautiful description',
-    image: Picture4,
+    image: Picture8,
   },
   {
     id: '9',
     title: 'Beautiful cake 4',
     description: 'A beautiful description',
-    image: Picture2,
+    image: Picture9,
   },
   {
     id: '10',
     title: 'Beautiful cake 2',
     description: 'A beautiful description',
-    image: Picture4,
+    image: Picture10,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture11,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture12,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture13,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture14,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture15,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture16,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture17,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture18,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture19,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture20,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture21,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture22,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture23,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture24,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture25,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture26,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture27,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture28,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture29,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture30,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture31,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture32,
+  },
+  {
+    id: '10',
+    title: 'Beautiful cake 2',
+    description: 'A beautiful description',
+    image: Picture33,
   },
 ]
